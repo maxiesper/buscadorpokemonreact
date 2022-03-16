3a202f865d89bab2a8c3fd80dad64a7f7c2ac668
maxiesper <mesper901@gmail.com>
2022-03-15 22:40:47 -0300

    pokedex creado con react

---
+++ src/Components/Header.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import icon from "../assets/roll.png";
+import { createUseStyles, useTheme } from "react-jss";
+
+
+const useStyle = createUseStyles(({ theme }) => ({
+    //(B)
+    "@global": {
+      html: {
+        backgroundColor: theme.background,
+      },
+    },
+    container: {
+      backgroundColor: theme.quaternary,
+      color: theme.color,
+      position: "fixed",
+      top: 0,
+      left: 0,
+      width: "100%",
+      textAlign: "right",
+      boxShadow: `${theme.boxShadow} ${theme.shadow}`,
+      "& img": {
+        margin: "0.2rem",
+        padding: "0.5rem",
+        width: "1.5rem",
+        cursor: "pointer",
+        "&:hover": {
+          backgroundColor: theme.tertiary,
+        },
+      },
+    },
+  }));
+
+const Header = () => {
+    const { toogleTheme } = useTheme();
+
+  const classes = useStyle();
+  return (
+    <div className={classes.container}>
+        <img src={icon} onClick={toogleTheme} alt="iconTheme"/>
+    </div>
+  );
+};
+
+export default Header;
